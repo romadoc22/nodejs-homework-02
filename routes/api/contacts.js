917f9abb4b2af95597926d1cb6527628bd8714bc
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-07-18 12:39:24 +0300

    додав роути

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,65 +1,26 @@
-const fs = require("fs").promises;
-const path = require("path");
-const { nanoid } = require("nanoid");
+const express = require("express");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const router = express.Router();
 
-const listContacts = async () => {
-  // Повертає масив контактів
-  const buffer = await fs.readFile(contactsPath);
-
-  return JSON.parse(buffer);
-};
-
-const getContactById = async (contactId) => {
-  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  const contacts = await listContacts();
-
-  return contacts.find(({ id }) => id === contactId) || null;
-};
-
-const removeContact = async (contactId) => {
-  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-  const contacts = await listContacts();
-  const index = contacts.findIndex(({ id }) => id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const [contactRemoved] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  return contactRemoved;
-};
-
-const addContact = async (body) => {
-  // Повертає об'єкт доданого контакту.
-  const contacts = await listContacts();
-  const contactAdded = { id: nanoid(), ...body };
-  contacts.push(contactAdded);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  return contactAdded;
-};
-
-const updateContact = async (contactId, data) => {
-  // Повертає об'єкт оновленого контакту.
-  const contacts = await listContacts();
-  const index = contacts.findIndex(({ id }) => id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const [contactFinded] = contacts.splice(index, 1);
-  const contactUpdated = { ...contactFinded, ...data };
-  contacts.push(contactUpdated);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  return contactUpdated;
-};
-
-module.exports = {
-  listContacts,
+const {
+  getContacts,
   getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+  deleteContact,
+  postContact,
+  putContact,
+} = require("../../controllers/controllers");
+
+const { schemaContactValidator } = require("../../schemas");
+const { validateBody } = require("../../middlewares");
+
+router.get("/", getContacts);
+
+router.get("/:id", getContactById);
+
+router.post("/", validateBody(schemaContactValidator), postContact);
+
+router.delete("/:id", deleteContact);
+
+router.put("/:id", validateBody(schemaContactValidator), putContact);
+
+module.exports = router;
