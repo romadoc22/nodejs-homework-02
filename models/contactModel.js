7370bca78f2df7143bae062f8b887e5b783c7e8f
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-07-27 10:26:15 +0300

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,18 +1,40 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
+const nameRegexp = /^[\p{Script=Latin}\p{Script=Cyrillic}\s]*$/u;
+const phoneRegexp = /^[0-9 ()+-]+$/;
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 // Валідатор даних на сервері
 const schemaDBContactValidator = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Set name for contact"],
+      validate: {
+        validator: function (name) {
+          return nameRegexp.test(name);
+        },
+        message: 'name is not a valid!',
+      },
+      required: [true, 'Set name for contact'],
     },
     email: {
       type: String,
+      validate: {
+        validator: function (email) {
+          return emailRegexp.test(email);
+        },
+        message: 'email is not a valid!',
+      },
     },
     phone: {
       type: String,
+      validate: {
+        validator: function (phone) {
+          return phoneRegexp.test(phone);
+        },
+        message: 'phone is not a valid!',
+      },
     },
     favorite: {
       type: Boolean,
@@ -32,9 +54,15 @@ const Contact = model("contact", schemaDBContactValidator);
 
 // Валідатори отриманих з клієнта даних
 const contactValidator = Joi.object({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "any.required": 'missing required name field',
+  }),
+  email: Joi.string().required().messages({
+    "any.required": 'missing required email field',
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": 'missing required phone field',
+  }),
   favorite: Joi.boolean(),
 });
 
